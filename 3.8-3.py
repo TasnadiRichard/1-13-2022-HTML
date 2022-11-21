5bdbd3c35bb35a44f9404f1f58c8c928d2dc915d
TasnadiRichard <112942145+TasnadiRichard@users.noreply.github.com>
2022-11-21 17:26:12 +0100

    Delete 3.8-3.py

--- 3.8-3.py
+++
@@ -1,4 +0,0 @@
-honapok = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"]
-
-for honap in honapok:
-    print(f"Az év egyik hónapja {honap}")
