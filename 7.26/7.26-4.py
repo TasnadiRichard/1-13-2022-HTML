ece198f1075e4003d5a8a5c6396a59c14a195eb4
TasnadiRichard <112942145+TasnadiRichard@users.noreply.github.com>
2023-01-10 08:02:43 +0100

    Delete 7.26 directory

--- 7.26/7.26-4.py
+++
@@ -1,6 +0,0 @@
-def ezOtBetusQ(szavak):
-   szam = 0
-   for szo in szavak:
-      if szo.len() == 5:
-         szam += 1
-   return szam
--- 7.26/7.26-9.py
+++
@@ -1,4 +0,0 @@
-def haromszogszamok(n):
-   return (n*(n+1))/2
-
-print(int(haromszogszamok(int(input()))))
